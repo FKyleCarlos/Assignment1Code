7164590c2c49a094b07e23869c39e0de20e803a7
Sean Sung-Won Lee <seanlee423@gmail.com>
2025-10-14 16:58:35 -0600

    Update Pyramid.java

--- src/shapes/Pyramid.java
+++ src/shapes/Pyramid.java
@@ -1,6 +1,34 @@
 package shapes;
 
-public class Pyramid
+public class Pyramid extends Shape
 {
+	
+	//attribute
+	private double side;
+
+	public Pyramid(double height, double side) 
+	{
+		super(height);
+		this.side = side;
+	}
+	
+	//getter
+	public double getSide()
+	{
+		return side;
+	}
+
+	@Override
+	public double calcVolume() 
+	{
+		return (1.0 / 3.0) * calcBaseArea() * getHeight();
+	}
+
+	@Override
+	public double calcBaseArea() 
+	{
+		return Math.pow(side,  2);
+	}
+	
 
 }
