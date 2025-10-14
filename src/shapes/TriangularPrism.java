f7016c1164e1a506bc114e9dd0de549f89f50af6
Sean Sung-Won Lee <seanlee423@gmail.com>
2025-10-14 16:58:58 -0600

    Update TriangularPrism.java

--- src/shapes/TriangularPrism.java
+++ src/shapes/TriangularPrism.java
@@ -1,6 +1,19 @@
 package shapes;
 
-public class TriangularPrism
+public class TriangularPrism extends Prism
 {
+	//constructor 
+	public TriangularPrism(double height, double side) 
+	{
+		super(height, side);
+	}
+	
+	@Override
+	public double calcBaseArea()
+	{
+		double side = getSide();
+		return (Math.pow(side, 2) * Math.sqrt(3)) /4;
+	}
 
 }
+
