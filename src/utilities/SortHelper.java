70d1f60294d98c2956faebe6e82bff1d20bf68f4
FKyleCarlos <francis.kylecarlos@gmail.com>
2025-10-15 17:42:43 -0600

    Updated Heapify and HeapSort to accommodate for generic datatype

--- src/utilities/SortHelper.java
+++ src/utilities/SortHelper.java
@@ -4,46 +4,46 @@ import java.util.Comparator;
 
 public class SortHelper
 {
-	static void Heapify(int arr[], int n, int i)
+	static <T> void Heapify(T[] arr, int n, int i, Comparator<T> comp)
 	{
 		int largest = i;
 		int left = (2 * i) + 1;
 		int right = (2 * i) + 2;
 		
-		if(left < n && arr[left] > arr[largest])
+		if(left < n && comp.compare(arr[left], arr[largest]) > 0)
 		{
 			largest = left;
 		}
 		
-		if(right < n && arr[right] > arr[largest])
+		if(right < n && comp.compare(arr[right], arr[largest]) > 0)
 		{
 			largest = right;
 		}
 		
 		if(largest != i)
 		{
-			int temp = arr[i];
+			T temp = arr[i];
 			arr[i] = arr[largest];
 			arr[largest] = temp;
-			Heapify(arr, n, largest);
+			Heapify(arr, n, largest, comp);
 		}
 	}
 	
-	static void HeapSort(int arr[])
+	static <T> void HeapSort(T[] arr, Comparator<T> comp)
 	{
 		int n = arr.length;
 		
 		for(int i = n/2 - 1; i >= 0; i--)
 		{
-			Heapify(arr, n, i);
+			Heapify(arr, n, i, comp);
 		}
 		
 		for(int i = n - 1; i > 0; i--)
 		{
-			int temp = arr[0];
+			T temp = arr[0];
 			arr[0] = arr[i];
 			arr[i] = temp;
-			Heapify(arr, i, 0);
+			Heapify(arr, i, 0, comp);
 		}
 	}
 	
