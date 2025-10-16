005f6a26dd84eda2ca3df42b1bf29d78f95be36e
Sean Sung-Won Lee <seanlee423@gmail.com>
2025-10-16 11:15:50 -0600

    Remove HeapSort and Heapify methods
    Removed HeapSort and Heapify methods from SortHelper class. and just added selection sorting

--- src/utilities/SelectionSort.java
+++ src/utilities/SelectionSort.java
@@ -4,50 +4,6 @@ import java.util.Comparator;
 
 public class SortHelper
 {
-	static void Heapify(int arr[], int n, int i)
-	{
-		int largest = i;
-		int left = (2 * i) + 1;
-		int right = (2 * i) + 2;
-		
-		if(left < n && arr[left] > arr[largest])
-		{
-			largest = left;
-		}
-		
-		if(right < n && arr[right] > arr[largest])
-		{
-			largest = right;
-		}
-		
-		if(largest != i)
-		{
-			int temp = arr[i];
-			arr[i] = arr[largest];
-			arr[largest] = temp;
-			Heapify(arr, n, largest);
-		}
-	}
-	
-	static void HeapSort(int arr[])
-	{
-		int n = arr.length;
-		
-		for(int i = n/2 - 1; i >= 0; i--)
-		{
-			Heapify(arr, n, i);
-		}
-		
-		for(int i = n - 1; i > 0; i--)
-		{
-			int temp = arr[0];
-			arr[0] = arr[i];
-			arr[i] = temp;
-			Heapify(arr, i, 0);
-		}
-	}
-	
-	
 	//Selection sorting by height****
 	public static <T extends Comparable<T>> void selectionSort(T[] array) 
 	{
@@ -88,5 +44,4 @@ public class SortHelper
             array[minIndex] = temp;
         }
     }
-	
 }
