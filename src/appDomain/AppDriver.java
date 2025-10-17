599501a1ac560c7e78f1aabe4bf93b934d3ab8fd
FKyleCarlos <francis.kylecarlos@gmail.com>
2025-10-16 21:29:01 -0600

    Updated files to fit testing all shapes in appDriver
    Updated SortHelper to HeapSort
    
    Commented void main from ShapeFileReader

--- src/appDomain/AppDriver.java
+++ src/appDomain/AppDriver.java
@@ -1,5 +1,10 @@
 package appDomain;
 
+import java.util.Comparator;
+import shapes.Shape;
+import shapes.ShapeFileReader;
+import utilities.*;
+
 /**
  * <p>
  * This application driver code is designed to be used as a basis for the
@@ -31,6 +36,79 @@ public class AppDriver
 		// refer to demo02 KittySort.java on how to use a custom sorting
 		// algorithm on a list of comparables to sort using either the
 		// natural order (comparable) or other orders (comparators)
+		
+		// Example arguments
+		String fileName = "res/shapes3.txt";
+
+		char compareType = 'v'; // v = volume, h = height, a = base area
+		char sortType = 'h';    // m = merge, q = quick
+
+		// Load shapes from file
+		Shape[] shapes = ShapeFileReader.readShapesFromFile(fileName);
+
+		if (shapes == null || shapes.length == 0) {
+			System.out.println("No shapes loaded from file.");
+			return;
+		}
+
+		// Choose comparator based on compareType
+		Comparator<Shape> cmp;
+		switch (compareType) {
+			case 'h':
+				cmp = (s1, s2) -> Double.compare(s1.getHeight(), s2.getHeight());
+				break;
+			case 'a':
+				cmp = (s1, s2) -> Double.compare(s1.calcBaseArea(), s2.calcBaseArea());
+				break;
+			case 'v':
+				cmp = (s1, s2) -> Double.compare(s1.calcVolume(), s2.calcVolume());
+				break;
+			default:
+				System.out.println("Invalid compare type. Use h, a, or v.");
+				return;
+		}
+
+		
+		long start = System.currentTimeMillis();
+
+		switch (sortType) {
+		    case 'b':
+		        utilities.BubbleSort.sort(shapes, cmp);
+		        System.out.println("\nSorted using Bubble Sort");
+		        break;
+		    case 'i':
+		        utilities.InsertionSort.sort(shapes, cmp);
+		        System.out.println("\nSorted using Insertion Sort");
+		        break;
+		    case 'm':
+		        utilities.MergeSort.sort(shapes, cmp);
+		        System.out.println("\nSorted using Merge Sort");
+		        break;
+		    case 'q':
+		        utilities.QuickSort.sort(shapes, cmp);
+		        System.out.println("\nSorted using Quick Sort");
+		        break;
+		    case 's':
+		        utilities.QuickSort.sort(shapes, cmp);
+		        System.out.println("\nSorted using Selection Sort");
+		        break;
+		    case 'h':
+		        utilities.HeapSort.sort(shapes, cmp);
+		        System.out.println("\nSorted using Heap Sort");
+		        break;
+		    default:
+		        System.out.println("Unknown sort type: " + sortType);
+		        return;
+		}
+
+		long end = System.currentTimeMillis();
+		System.out.println("Time taken: " + (end - start) + " ms");
+
+		System.out.println("\nFirst Shape: " + shapes[0].getClass().getSimpleName() +
+				" | Area: " + shapes[0].calcVolume());
+
+		System.out.println("Last Shape: " + shapes[shapes.length - 1].getClass().getSimpleName() +
+				" | Area: " + shapes[shapes.length - 1].calcVolume());
 
 	}
 
